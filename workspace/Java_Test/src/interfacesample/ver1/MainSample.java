7cf20bac6d956c16a155a0fe8202d587fd77724c
SAJ09210004\SAJ09210004 <good.dhkim@gmail.com>
2021-04-28 13:51:53 +0900

    [Add] Interface 인터페이스에 대한 수준낮은 고찰

---
+++ workspace/Java_Test/src/interfacesample/ver1/MainSample.java
@@ -0,0 +1,39 @@
+package interfacesample.ver1;
+
+public class MainSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		BusTmp bus = new BusTmp();
+		 	System.out.printf("Bus Sound : %s \n",bus.sound());
+		 	
+		CarTmp car = new CarTmp();
+			System.out.printf("Car Sound : %s \n",car.sound());			
+			
+		BikeTmp bike = new BikeTmp();
+		    System.out.printf("Bike Sound : %s \n",bike.sound());	
+
+	}	
+}
+
+// Bus Sound
+class BusTmp{
+	public String sound() {
+		return "빠 ~~~앙";
+	}
+}
+
+// Car Sound
+class CarTmp{
+	public String sound() {
+		return "빵 빵";
+	}
+}
+
+// Bike Sound
+class BikeTmp{
+	public String sound() {
+		return "따릉 따릉";
+	}
+}
