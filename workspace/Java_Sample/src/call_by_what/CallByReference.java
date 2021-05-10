8b72f2e17238f610f2a2a6b3db8ca6bfbd30633a
SAJ09210004\SAJ09210004 <good.dhkim@gmail.com>
2021-05-10 13:32:53 +0900

    Call_by_what

---
+++ workspace/Java_Sample/src/call_by_what/CallByReference.java
@@ -0,0 +1,28 @@
+package call_by_what;
+
+public class CallByReference {
+	
+	int value;
+
+	public static void main(String[] args) {		
+		CallByReference a = new CallByReference(10);
+		CallByReference b = new CallByReference(20);
+
+		System.out.println("swap() 호출 전 : a = " + a.value + ", b = " + b.value);
+		swap(a, b);
+		
+		System.out.println("swap() 호출 전 : a = " + a.value + ", b = " + b.value);
+	}
+	
+	
+	CallByReference(int value) {
+		this.value = value;
+	}
+
+	public static void swap(CallByReference x, CallByReference y) {
+		int temp = x.value;
+		x.value = y.value;
+		y.value = temp;
+	}
+	
+}
