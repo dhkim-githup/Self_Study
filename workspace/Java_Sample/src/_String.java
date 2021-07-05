d9944bc6ea9bb963009037fd625fb09c1a5d9bea
NAH01080009\NAH01080009 <good.dhkim@gmail.com>
2021-07-05 13:55:17 +0900

    File Up

--- workspace/Java_Sample/src/_String.java
+++ workspace/Java_Sample/src/_String.java
@@ -43,7 +43,24 @@ public class _String  {
 	       sb2.append("홍길동");
 	System.out.printf("sb2.hashcode %s \n", System.identityHashCode(sb2));
 	
-	
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
 	}	
 	  
 }
--- workspace/SpringSampleMaven/src/main/java/com/study/demo/_team/aop/book/aop002/MyAspect.java
+++ workspace/SpringSampleMaven/src/main/java/com/study/demo/_team/aop/book/aop002/MyAspect.java
@@ -9,11 +9,16 @@ public class MyAspect {
 	
     /* 특정 위치(패키지)의 메소드에서만 실행 */
 	//@Before("execution(public void com.study.demo._team.aop.book.aop002.Boy.runSomething())")
-    
+   
     /* 해당 메서드는 모두 적용 */
-    @Before("execution(* runSomething())") // 모든 메서드 runSomething 을 사용하는 모든 위치
+    //@Before("execution(* runSomething())") // 모든 메서드 runSomething 을 사용하는 모든 위치
+
+	/* Proxy pattern 사용 */ 
+	//@Before("execution(public void com.study.demo._team.aop.book.aop002.Person.runSomething())")
+	
+	 @Before("execution(public void com.study.demo._team.aop.book.aop002.Boy.runSomething())")
 	 public void before(JoinPoint joinPoint){
-	  System.out.println("aop002.얼굴 인식 확인: 문을 개방하라");
+	  System.out.println("aop002.MyAspect. 얼굴 인식 확인: 문을 개방하라");
 	  //System.out.println("열쇠로 문을 열고 집에 들어간다.");
 	 }
 
