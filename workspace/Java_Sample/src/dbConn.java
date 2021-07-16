4c836af0f8886161a74a517a3fc39e6645db7eff
SAJ09210004\SAJ09210004 <good.dhkim@gmail.com>
2021-07-16 14:38:48 +0900

    File Up

--- workspace/Java_Sample/src/dbConn.java
+++ workspace/Java_Sample/src/dbConn.java
@@ -10,15 +10,18 @@ public class dbConn {
    // 1. jdbc driver 를 등록한다.
    try {
 	Class.forName("oracle.jdbc.driver.OracleDriver");
-    conn = DriverManager.getConnection(
-           "jdbc:oracle:thin:@192.168.1.211:1521:TESTDB","METS_IMSI","METS_IMSI");
-    infile= "select *  from all_users";
+    //conn = DriverManager.getConnection("jdbc:oracle:thin:@172.16.3.24:1521:TESTDB","METS18940G","METS18940G070%");
+	
+	/* 크라운제과 */
+    conn = DriverManager.getConnection("jdbc:oracle:thin:@123.140.106.166:1521:SALE","IMD000","IMD000");
+    
+    infile= "select to_char(sysdate,'yyyy.mm.dd') as time  from dual";
 
   Statement stmt = conn.createStatement();
   ResultSet srs = stmt.executeQuery(infile);
   while (srs.next()) {
           name = srs.getString(1);
-          System.out.println( "cust_ID:"+name);
+          System.out.println( "time:"+name);
   }
       System.out.println("셀렉트 완성");
       conn.close();
