2ae5a115e3b53d750cc7c01265541607e026a145
SAJ09210004\SAJ09210004 <good.dhkim@gmail.com>
2021-08-13 18:34:44 +0900

    File Up

---
+++ workspace/SpringSilgleton/src/main/java/com/spring/singleton/Main3.java
@@ -0,0 +1,59 @@
+package com.spring.singleton;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Main3 {
+
+	@Autowired
+	ServiceNormal serviceNormal;
+	
+	
+	@RequestMapping("/normal3")
+	public String doNormal() {			
+		
+		String name;
+		
+		System.out.println("");
+		
+		name = serviceNormal.getName();		
+		System.out.println("name1 =>"+name);
+		
+		serviceNormal.setName("홍길순");
+		
+		name = serviceNormal.getName();		
+		System.out.println("name2 =>"+name);
+		
+		
+		return name;
+	}
+	
+	@RequestMapping("/test3")
+	public String doTest(HttpServletRequest request) {
+		
+		ServiceTest serviceTest = new ServiceTest();
+		
+		System.out.println("");
+		
+		System.out.println("ServiceNormal =>"+serviceNormal);
+		System.out.println("serviceTest =>"+serviceTest);
+		
+		String reName = request.getParameter("name");
+		String name;
+		
+		name = serviceTest.getName();		
+		System.out.println("name1 =>"+name);
+		
+		serviceTest.setName(reName);
+		
+		name = serviceTest.getName();		
+		System.out.println("name2 =>"+name);
+		
+		
+		return name;
+	}
+}
--- workspace/SpringSilgleton/src/main/java/com/spring/singleton/ServiceTest.java
+++ workspace/SpringSilgleton/src/main/java/com/spring/singleton/ServiceTest.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(value = "prototype")
+@Scope("prototype")
 public class ServiceTest {
 	
 	String name="이성계";
