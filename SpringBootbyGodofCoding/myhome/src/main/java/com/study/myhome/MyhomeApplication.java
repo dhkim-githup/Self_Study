ecd06e1bdeb3ef68e9a7d27ced88d6c495c6a6c8
DESKTOP-UEGKJGF\PC <good.dhkim@gmail.com>
2022-02-18 22:21:54 +0900

    init

--- SpringBootbyGodofCoding/myhome/src/main/java/com/study/myhome/MyhomeApplication.java
+++ SpringBootbyGodofCoding/myhome/src/main/java/com/study/myhome/MyhomeApplication.java
@@ -2,6 +2,7 @@ package com.study.myhome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class MyhomeApplication {
