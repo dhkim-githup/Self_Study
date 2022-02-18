2d767e08a809cfc0a3275fe3c2b25ea9ba9b57a9
DESKTOP-UEGKJGF\PC <good.dhkim@gmail.com>
2022-02-18 22:12:29 +0900

    init

--- workspace/SpringBeanScope/src/main/java/com/spring/scope/Main1.java
+++ workspace/SpringBeanScope/src/main/java/com/spring/scope/Main1.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Lazy
 public class Main1 {
 
 	@Autowired
--- workspace/SpringBeanScope/src/main/java/com/spring/scope/beanlist.java
+++ workspace/SpringBeanScope/src/main/java/com/spring/scope/beanlist.java
@@ -2,6 +2,7 @@ package com.spring.scope;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,9 @@ public class beanlist {
 
 	@Autowired
 	DefaultListableBeanFactory df;
+	
+	@Autowired
+    private ApplicationContext applicationContext;
 
 	@RequestMapping("/mybean")
 	public String myBean() {
@@ -24,4 +28,25 @@ public class beanlist {
 		
 		return "Total : "+count+"<p>"+strBean;
 	}
+	
+	@RequestMapping("/mybean2")
+    public String contextLoads() throws Exception {
+		String strBean="";
+		int count=0;
+        if (applicationContext != null) {
+            String[] beans = applicationContext.getBeanDefinitionNames();
+
+            for (String bean : beans) {
+                System.out.println("bean : " + bean);
+                strBean += bean + "<br>";  
+    			count++;
+            }
+        }
+        
+        return "Total : "+count+"<p>"+strBean;
+        
+    }
+	
+	
+	
 }
