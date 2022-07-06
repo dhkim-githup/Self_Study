d3f4a9b5e76c5f3ab789380d70dbe59a12cb9964
DESKTOP-UEGKJGF\PC <good.dhkim@gmail.com>
2022-07-07 07:40:03 +0900

    .

--- workspace/Springboot_AWS_book/src/test/java/com/example/aws/web/HelloControllerTest.java
+++ workspace/Springboot_AWS_book/src/test/java/com/example/aws/web/HelloControllerTest.java
@@ -1,9 +1,14 @@
 package com.example.aws.web;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HelloControllerTest {
 
     HelloController helloController = new HelloController();
