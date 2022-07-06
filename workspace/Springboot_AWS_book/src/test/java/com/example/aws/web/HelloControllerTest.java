f9da3ae598c50caf9ad270a604837c9a9cdde4c3
DESKTOP-UEGKJGF\PC <good.dhkim@gmail.com>
2022-07-07 07:03:23 +0900

    File Up

--- workspace/Springboot_AWS_book/src/test/java/com/example/aws/web/HelloControllerTest.java
+++ workspace/Springboot_AWS_book/src/test/java/com/example/aws/web/HelloControllerTest.java
@@ -1,18 +1,23 @@
 package com.example.aws.web;
 
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.Test;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
-public class HelloControllerTest {
+import static org.junit.jupiter.api.Assertions.*;
 
-    @Autowired
-    private MockMvc mockMvc; //null?
+class HelloControllerTest {
 
+    HelloController helloController = new HelloController();
+
+    @Test
+    void hello() {
+
+        String expected  = "hello";
+
+        String actual  = helloController.hello();
+
+        System.out.println("Print : "+actual );
+
+        assertEquals(expected , actual );
+
+    }
 }
