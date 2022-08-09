49d4000d735c9abb9e5c25323dd069729bfa5dd9
DESKTOP-UEGKJGF\PC <good.dhkim@gmail.com>
2022-08-09 22:19:07 +0900

    File Up

---
+++ SpringbootRecordV2/src/main/java/com/example/bootv2/BootV2Application.java
@@ -0,0 +1,13 @@
+package com.example.bootv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootV2Application.class, args);
+    }
+
+}
