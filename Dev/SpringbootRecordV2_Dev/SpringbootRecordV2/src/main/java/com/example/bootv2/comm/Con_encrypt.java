92f1faf2c63fe2d8c8e2c584621f591660fb811a
SAJ09210004\SAJ09210004 <good.dhkim@gmail.com>
2022-08-24 18:11:10 +0900

    File Up

--- Dev/SpringbootRecordV2_Dev/SpringbootRecordV2/src/main/java/com/example/bootv2/comm/Con_encrypt.java
+++ Dev/SpringbootRecordV2_Dev/SpringbootRecordV2/src/main/java/com/example/bootv2/comm/Con_encrypt.java
@@ -1,21 +1,44 @@
 package com.example.bootv2.comm;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+/**
+ * 단방향 , 양방향 암호화 진행
+ * @author IT늦공
+ * @see <a href="https://m.mkexdev.net/441">개인정보보호문서 - https://m.mkexdev.net/441</a>
+ * @see <a href="https://1-7171771.tistory.com/82">단방향 참고 블로그 https://1-7171771.tistory.com/82</a>
+ * @see <a href="https://fknd12.tistory.com/76">단방향/양방향 참고 블로그 https://fknd12.tistory.com/76</a>
+ */
 
 public class Con_encrypt {
 
     /* 양방향 */
 
-    /* 단방향  https://1-7171771.tistory.com/82
-    */
+
+    /**
+     * 단방향 암호화 SHA-256
+     * @param s  암호화할 값 입력
+     * @return   string 암호화된 값 리턴
+     */
     public String encryptSHA256(String s) {
         return encrypt(s, "SHA-256");
     }
+
 
     public String encryptMD5(String s) {
         return encrypt(s, "MD5");
     }
 
+    /**
+     * 단방향 암호화
+     * @param s 암호화할 문자
+     * @param messageDigest 암호화 방식 SHA-256 or MD5
+     * @return
+     */
     public String encrypt(String s, String messageDigest) {
         try {
             MessageDigest md = MessageDigest.getInstance(messageDigest);
@@ -29,4 +52,65 @@ public class Con_encrypt {
             return s;
         }
     }
+
+    /**
+     * 단방향 암호화 Sha256
+     * @param value
+     * @return string 암호화된 값
+     */
+    public String encryptSha256(String value) {
+
+        MessageDigest md;
+        StringBuffer sb = new StringBuffer("");
+
+        try {
+
+            md = MessageDigest.getInstance("SHA-256");
+            md.update(value.getBytes());
+            byte byteData[] = md.digest();
+
+            for (byte tmpStrByte : byteData) {
+                String tmpEncTxt = Integer.toString((tmpStrByte & 0xff) + 0x100, 16).substring(1);
+
+                sb.append(tmpEncTxt);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * 양방향 암호화 - 대칭키
+     * @param str 암호화 문자열
+     * @param key 암호화 키 (16,24,32 byte ex : AB123ab456CDcd@#)
+     * @return
+     * @throws Exception
+     */
+    public static String encryptAes(String str, String key) throws Exception {
+
+        Cipher cipher = Cipher.getInstance("AES");
+        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encPassword = cipher.doFinal(str.getBytes("UTF-8"));
+        String result = Base64.getEncoder().encodeToString(encPassword);
+
+        return result;
+    }
+
+    public static String decryptAes(String str, String key) throws Exception {
+
+        Cipher cipher = Cipher.getInstance("AES");
+        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
+
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
+        byte[] decPassword = cipher.doFinal(Base64.getDecoder().decode(str));
+        String result = new String(decPassword, "UTF-8");
+
+        return result;
+    }
+
 }
--- Dev/SpringbootRecordV2_Dev/SpringbootRecordV2/src/test/java/com/example/bootv2/comm/Con_encryptTest.java
+++ Dev/SpringbootRecordV2_Dev/SpringbootRecordV2/src/test/java/com/example/bootv2/comm/Con_encryptTest.java
@@ -7,16 +7,46 @@ import static org.junit.jupiter.api.Assertions.*;
 class Con_encryptTest {
 
     @Test
-    public void doTest(){
+    public void doTest() throws Exception {
         Con_encrypt conEncrypt = new Con_encrypt();
 
         String strName="1234";
+        String strName2="홍길동이너네아버지이냐 우리 아버지이냐 그것이 궁금하다";
+        String strKey16="AB123ab456CDcd@#";
+        String strEnc16="Utx45XL8ETGHrl6nOMK2ZQ==";
+        String strKey32="AB123ab456CDcd@#AB123ab456CDcd@#";
+        String strEnc32="V4V5t6BAX3oLw0RNbgb+Rw==";
+        String strEnc32a="khScc4bk7YFzbO4RETPl0l7jAAbgKOO87P9//lc0oq7d5AM6BsdCTMawl+9Dn5Fy8Tl/meHkd1sOMbdtWod7/W7cTMR6yB/NxKbvS3D1YnE=";
+
         String strNameEncrypt="";
 
+        /* 단반향암호화 */
         strNameEncrypt = conEncrypt.encryptSHA256(strName);
         System.out.println("strNameEncrypt : "+strNameEncrypt);
 
-        assertEquals("03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4",strNameEncrypt);
+        //assertEquals("03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4",strNameEncrypt);
+
+        /* 양방향 암호화 */
+        strNameEncrypt = conEncrypt.encryptSha256(strName);
+        System.out.println("strNameEncrypt : "+strNameEncrypt);
+
+        strNameEncrypt = conEncrypt.encryptAes(strName,strKey16);
+        System.out.println("strNameEncrypt 16: "+strNameEncrypt);
+
+        strNameEncrypt = conEncrypt.decryptAes(strEnc16,strKey16);
+        System.out.println("strNameEncrypt 16: "+strNameEncrypt);
+
+        strNameEncrypt = conEncrypt.encryptAes(strName,strKey32);
+        System.out.println("strNameEncrypt 32: "+strNameEncrypt);
+
+        strNameEncrypt = conEncrypt.decryptAes(strEnc32,strKey32);
+        System.out.println("strNameEncrypt 32: "+strNameEncrypt);
+
+        strNameEncrypt = conEncrypt.encryptAes(strName2,strKey32);
+        System.out.println("strNameEncrypt 32: "+strNameEncrypt);
+        strNameEncrypt = conEncrypt.decryptAes(strEnc32a,strKey32);
+        System.out.println("strNameEncrypt 32: "+strNameEncrypt);
+
 
 
     }
