02f292da39d3ec9096fee692a28874084c0b3e3e
SAJ09210004\SAJ09210004 <good.dhkim@gmail.com>
2022-09-02 15:45:58 +0900

    File Up

---
+++ Dev/WebBeginning/CoffeeDev/src/main/java/com/example/coffeedev/CoffeeDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffeedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeDevApplication.class, args);
+    }
+
+}
---
+++ Dev/WebBeginning/CoffeeDev/src/test/java/com/example/coffeedev/CoffeeDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffeedev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
