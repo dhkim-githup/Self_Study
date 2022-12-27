5cb0785ae7897f76292b4bf919a8e913d91165c3
DESKTOP-RTMQGFQ\USER <good.dhkim@gmail.com>
2022-12-28 01:17:12 +0900

    File Up

---
+++ IT_ERP/SampleTest/src/test/java/com/example/sampletest/SampleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampletest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
