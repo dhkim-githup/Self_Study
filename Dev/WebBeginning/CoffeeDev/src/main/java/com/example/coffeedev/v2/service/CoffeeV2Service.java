c60c743cafcce043d7bce865ed0b2848cd622161
DESKTOP-RTMQGFQ\USER <good.dhkim@gmail.com>
2023-02-06 22:36:52 +0900

    react up

--- Dev/WebBeginning/CoffeeDev/src/main/java/com/example/coffeedev/v2/service/CoffeeV2Service.java
+++ Dev/WebBeginning/CoffeeDev/src/main/java/com/example/coffeedev/v2/service/CoffeeV2Service.java
@@ -23,8 +23,6 @@ import java.util.Map;
 @Log4j2
 public class CoffeeV2Service {
 
-
-
     @Autowired
     CoffeeV2Dao v2Dao;
 
---
+++ React/생활코딩2022개정판/my-app/src/App.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header(props){
+  
+  return <header>
+         <h1><a href="/">{props.title}</a></h1>   
+         </header>   
+
+}
+
+function Nav(props){
+  const list= [];
+  for(let i=0; i<props.topics.length; i++){
+    let t=props.topics[i];
+    list.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>);
+  }
+  return <nav>
+          <ol>
+            {list}
+          </ol> 
+        </nav>
+}
+
+function Article(props){
+  console.log(props);
+  return  <article>
+            <h2>{props.title}</h2>
+              {props.body}
+          </article>
+}
+
+function App() {
+  const topics = [
+    {id:1, title:'html', body:'html is'},
+    {id:2, title:'css', body:'css is'},
+    {id:3, title:'js', body:'Javascript is'},
+    ]
+  return (
+    <div>
+      <Header title="ReactTitle"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Article Title" body="Hello Article"></Article> 
+      <Article title="Title" body="Hello Article"></Article> 
+    </div>
+  );
+}
+
+export default App;
