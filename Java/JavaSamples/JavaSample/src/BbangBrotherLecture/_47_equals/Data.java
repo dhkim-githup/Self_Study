8ff2f3b65ca34a6f8420facdb868a12f4c8094fd
NBC10160027\NBC10160027 <good.dhkim@gmail.com>
2023-12-05 17:31:36 +0900

    File Up

--- Java/JavaSamples/JavaSample/src/BbangBrotherLecture/_47_equals/Data.java
+++ Java/JavaSamples/JavaSample/src/BbangBrotherLecture/_47_equals/Data.java
@@ -12,14 +12,6 @@ public class Data {
     }
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        return result;
-    }
-
-    @Override
     public boolean equals(Object obj) {
         if (this == obj)
             return true;
